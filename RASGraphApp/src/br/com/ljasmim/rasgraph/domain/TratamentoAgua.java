cd03563984d11f7fc665ca9545140e9965241f8a
leojasmim <leojasmim@hotmail.com>
2016-08-01 20:58:47 -0300

    #27 Classe TratamentoAgua implementada

---
+++ RASGraphApp/src/br/com/ljasmim/rasgraph/domain/TratamentoAgua.java
@@ -0,0 +1,79 @@
+package br.com.ljasmim.rasgraph.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Classe para armazenar as entidades que representa o descricao de tratamento da
+ agua de consumo
+ *
+ * @author LeonardoJasmim
+ */
+@Entity
+public class TratamentoAgua implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "tipo")
+    private String descricao;
+
+    public TratamentoAgua() {
+    }
+
+    public TratamentoAgua(long id, String tipo) {
+        this.id = id;
+        this.descricao = tipo;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 73 * hash + (int) (this.id ^ (this.id >>> 32));
+        hash = 73 * hash + Objects.hashCode(this.descricao);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TratamentoAgua other = (TratamentoAgua) obj;
+        if (!Objects.equals(this.descricao, other.descricao)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "TratamentoAgua{" + "id=" + id + ", decricao=" + descricao + '}';
+    }
+
+}
