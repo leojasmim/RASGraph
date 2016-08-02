26aa8613be7a04645ff3ad5b009c3629ecd97ca0
leojasmim <leojasmim@hotmail.com>
2016-08-02 08:27:08 -0300

    #27 Classe MeioTransporte implementada

---
+++ RASGraphApp/src/br/com/ljasmim/rasgraph/domain/MeioTransporte.java
@@ -0,0 +1,76 @@
+package br.com.ljasmim.rasgraph.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Classe que representa um meio de transporte
+ *
+ * @author LeonardoJasmim
+ */
+@Entity
+public class MeioTransporte implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String descricao;
+
+    public MeioTransporte() {
+    }
+
+    public MeioTransporte(long id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + (int) (this.id ^ (this.id >>> 32));
+        hash = 67 * hash + Objects.hashCode(this.descricao);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MeioTransporte other = (MeioTransporte) obj;
+        if (!Objects.equals(this.descricao, other.descricao)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "MeioTransporte{" + "id=" + id + ", descricao=" + descricao + '}';
+    }
+
+}
