48b68e49026bde6e70e6b14e14c114789188f168
leojasmim <leojasmim@hotmail.com>
2016-08-02 22:44:13 -0300

    #32 Classe MeioComunicacaoDAO testada

---
+++ RASGraphApp/src/br/com/ljasmim/rasgraph/domain/MeioComunicacao.java
@@ -0,0 +1,77 @@
+package br.com.ljasmim.rasgraph.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Classe que reprensenta um meio de comunicação
+ *
+ * @author LeonardoJasmim
+ */
+@Entity
+public class MeioComunicacao implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Column(name = "descricao", unique = true)
+    private String descricao;
+
+    public MeioComunicacao() {
+    }
+
+    public MeioComunicacao(long id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + (int) (this.id ^ (this.id >>> 32));
+        hash = 83 * hash + Objects.hashCode(this.descricao);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MeioComunicacao other = (MeioComunicacao) obj;
+        if (!Objects.equals(this.descricao, other.descricao)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "MeioComunicacao{" + "id=" + id + ", descricao=" + descricao + '}';
+    }
+}
---
+++ RASGraphApp/test/br/com/ljasmim/rasgraph/dao/test/MeioComunicacaoTest.java
@@ -0,0 +1,76 @@
+package br.com.ljasmim.rasgraph.dao.test;
+
+import br.com.ljasmim.rasgraph.dao.MeioComunicacaoDAO;
+import br.com.ljasmim.rasgraph.domain.MeioComunicacao;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ * Teste de persistência da classe MeioComunicacaoDAO
+ *
+ * @author LeonardoJasmim
+ */
+public class MeioComunicacaoTest {
+    
+    MeioComunicacao meioComunicacao;
+    MeioComunicacaoDAO meioComunicacaoDAO;
+    List<MeioComunicacao> meiosComunicacao;
+    
+    public MeioComunicacaoTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        meioComunicacaoDAO = new MeioComunicacaoDAO();
+    }
+    
+    @Test
+    public void testSave() {
+        meioComunicacao = new MeioComunicacao();
+        meioComunicacao.setDescricao("MEIOCOMUN_01");
+        meioComunicacao = meioComunicacaoDAO.save(meioComunicacao);
+        assertTrue(meioComunicacao != null);
+        meioComunicacao = new MeioComunicacao();
+        meioComunicacao.setDescricao("MEIOCOMUN_02");
+        meioComunicacao = meioComunicacaoDAO.save(meioComunicacao);
+        assertTrue(meioComunicacao != null);
+        meioComunicacao = new MeioComunicacao();
+        meioComunicacao.setDescricao("MEIOCOMUN_03");
+        meioComunicacao = meioComunicacaoDAO.save(meioComunicacao);
+        assertTrue(meioComunicacao != null);
+        meioComunicacao = new MeioComunicacao();
+        meioComunicacao.setDescricao("MEIOCOMUN_03");
+        meioComunicacao = meioComunicacaoDAO.save(meioComunicacao);
+        assertTrue(meioComunicacao == null);
+    }
+    
+    @Test
+    public void testRemove() {
+        meioComunicacao = meioComunicacaoDAO.getByID(3L);
+        if (meioComunicacao != null) {
+            meioComunicacaoDAO.remove(meioComunicacao);
+        }
+        meioComunicacao = meioComunicacaoDAO.getByID(3L);
+        assertTrue(meioComunicacao == null);
+    }
+    
+    @Test
+    public void testGetAllAndCount() {
+        long count = meioComunicacaoDAO.getCount();
+        meiosComunicacao = meioComunicacaoDAO.getAll();
+        assertTrue(meiosComunicacao.size() == count);
+        assertTrue(count == 2);
+    }
+    
+    @Test
+    public void testFind(){
+        meioComunicacao = new MeioComunicacao();
+        meioComunicacao.setDescricao("MEIOCOMUN_05");
+        assertTrue(meioComunicacaoDAO.find(meioComunicacao) == null);
+        meioComunicacao.setDescricao("MEIOCOMUN_01");
+        assertTrue(meioComunicacaoDAO.find(meioComunicacao) != null);
+        assertTrue(meioComunicacaoDAO.find(meioComunicacao).getId() == 1);
+    }
+}
