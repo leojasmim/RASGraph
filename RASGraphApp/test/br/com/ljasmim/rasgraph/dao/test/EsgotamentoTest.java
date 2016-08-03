2b659b06ed22d74935f4e953212f635fa29ca03b
leojasmim <leojasmim@hotmail.com>
2016-08-02 22:13:11 -0300

    #32 Classe EsgotamentoDAO testada

---
+++ RASGraphApp/test/br/com/ljasmim/rasgraph/dao/test/EsgotamentoTest.java
@@ -0,0 +1,76 @@
+package br.com.ljasmim.rasgraph.dao.test;
+
+import br.com.ljasmim.rasgraph.dao.EsgotamentoDAO;
+import br.com.ljasmim.rasgraph.domain.Esgotamento;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ * Teste de persistência da classe Esgotamento
+ *
+ * @author LeonardoJasmim
+ */
+public class EsgotamentoTest {
+
+    Esgotamento esgotamento;
+    EsgotamentoDAO esgotamentoDAO;
+    List<Esgotamento> esgotamentos;
+
+    public EsgotamentoTest() {
+    }
+
+    @Before
+    public void setUp() {
+        esgotamentoDAO = new EsgotamentoDAO();
+    }
+
+    @Test
+    public void testSave(){
+        esgotamento = new Esgotamento();
+        esgotamento.setDescricao("ESGOTO_01");
+        esgotamento = esgotamentoDAO.save(esgotamento);
+        assertTrue(esgotamento != null);
+        esgotamento = new Esgotamento();
+        esgotamento.setDescricao("ESGOTO_02");
+        esgotamento = esgotamentoDAO.save(esgotamento);
+        assertTrue(esgotamento != null);
+        esgotamento = new Esgotamento();
+        esgotamento.setDescricao("ESGOTO_03");
+        esgotamento = esgotamentoDAO.save(esgotamento);
+        assertTrue(esgotamento != null);
+        esgotamento = new Esgotamento();
+        esgotamento.setDescricao("ESGOTO_03");
+        esgotamento = esgotamentoDAO.save(esgotamento);
+        assertTrue(esgotamento == null);
+    }
+        
+    @Test
+    public void testRemove(){
+        esgotamento = esgotamentoDAO.getByID(3L);
+        if (esgotamento != null){
+            esgotamentoDAO.remove(esgotamento);
+        }
+        esgotamento = esgotamentoDAO.getByID(3L);
+        assertTrue(esgotamento == null);
+    }
+    
+    @Test
+    public void testGetAllAndCount(){
+        long count = esgotamentoDAO.getCount();
+        esgotamentos = esgotamentoDAO.getAll();
+        assertTrue(esgotamentos.size() ==  count);
+        assertTrue(count == 2);
+    }
+    
+    @Test
+    public void testFind(){
+        esgotamento = new Esgotamento();
+        esgotamento.setDescricao("ESGOTO_05");
+        assertTrue(esgotamentoDAO.find(esgotamento) == null);
+        esgotamento.setDescricao("ESGOTO_01");
+        assertTrue(esgotamentoDAO.find(esgotamento).getId() == 1);
+    }
+            
+}
