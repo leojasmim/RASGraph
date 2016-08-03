8be280b8ae33a36496486fd24e8119ebb766b1f4
leojasmim <leojasmim@hotmail.com>
2016-08-02 22:29:50 -0300

    #32 Classe GrupoComunitarioDAO testada

---
+++ RASGraphApp/test/br/com/ljasmim/rasgraph/dao/test/GrupoComunitarioTest.java
@@ -0,0 +1,75 @@
+package br.com.ljasmim.rasgraph.dao.test;
+
+import br.com.ljasmim.rasgraph.dao.GrupoComunitarioDAO;
+import br.com.ljasmim.rasgraph.domain.GrupoComunitario;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ * Teste de persistência da classe GrupoComunitario
+ * @author LeonardoJasmim
+ */
+public class GrupoComunitarioTest {
+    
+    GrupoComunitario grupo;
+    GrupoComunitarioDAO grupoDAO;
+    List<GrupoComunitario> grupos;
+    
+    public GrupoComunitarioTest() {
+    }
+
+    @Before
+    public void setUp(){
+        grupoDAO = new GrupoComunitarioDAO();
+    }
+    
+    @Test
+    public void testSave(){
+        grupo = new GrupoComunitario();
+        grupo.setDescricao("GRUPOCOMUN_01");
+        grupo =  grupoDAO.save(grupo);
+        assertTrue(grupo != null);
+        grupo = new GrupoComunitario();
+        grupo.setDescricao("GRUPOCOMUN_02");
+        grupo =  grupoDAO.save(grupo);
+        assertTrue(grupo != null);
+        grupo = new GrupoComunitario();
+        grupo.setDescricao("GRUPOCOMUN_03");
+        grupo =  grupoDAO.save(grupo);
+        assertTrue(grupo != null);
+        grupo = new GrupoComunitario();
+        grupo.setDescricao("GRUPOCOMUN_03");
+        grupo =  grupoDAO.save(grupo);
+        assertTrue(grupo == null);
+    }
+        
+    @Test
+    public void testRemove(){
+        grupo = grupoDAO.getByID(3L);
+        if (grupo != null){
+            grupoDAO.remove(grupo);
+        }
+        grupo = grupoDAO.getByID(3L);
+        assertTrue(grupo == null);
+    }
+    
+    @Test
+    public void testGetAllAndCount(){
+        long count = grupoDAO.getCount();
+        grupos = grupoDAO.getAll();
+        assertTrue(grupos.size() == count);
+        assertTrue(count == 2);
+    }
+    
+    @Test
+    public void testFind(){
+        grupo = new GrupoComunitario();
+        grupo.setDescricao("GRUPOCOMUN_05");
+        assertTrue(grupoDAO.find(grupo) == null);
+        grupo.setDescricao("GRUPOCOMUN_01");
+        assertTrue(grupoDAO.find(grupo) != null);
+        assertTrue(grupoDAO.find(grupo).getId() == 1);
+    }
+}
